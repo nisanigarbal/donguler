4ddddff88b0b5aef3f32cddbbb6e87a34f37ead8
niss <143124801+nisanigarbal@users.noreply.github.com>
2024-09-07 17:05:28 +0300

    Update and rename armstrongnumbers.java to Armstrongnumbers.java

---
+++ Armstrongnumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int sum = 0;
+        int temp = number;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+
+        System.out.println("Sum of the digits of the number: " + sum);
+    }
+}
