8f23d4097c39ebdf90710f75e8999c601ecef80e
niss <143124801+nisanigarbal@users.noreply.github.com>
2024-09-07 17:11:20 +0300

    Update and rename harmonik.java to Harmonic-Numbers.java

--- Harmonic-Numbers.java
+++ Harmonic-Numbers.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-public class harmonik {
+public class harmonic-numbers {
     public static void main (String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.print(" N sayısını girin: ");
+        System.out.print(" enter N numbers: ");
         int n = scan.nextInt();
         double result = 0.0;
 
